853f74c3e812b09663188e1de06f424d62df6f65
rvemulapati <vemulapatirohini@gmail.com>
2020-06-10 15:31:51 -0500

    first commit

---
+++ nestedif.sh
@@ -0,0 +1,13 @@
+int1=$1
+int2=$2
+if [ $int1 -eq $int2 ]; then
+	echo "both are equal"
+else 
+	echo "both are not equal"
+		if [ $int1 -lt $int2 ]; then
+		echo "the values are lesser"
+			else 
+			echo "values are not valid "
+fi
+fi
+
